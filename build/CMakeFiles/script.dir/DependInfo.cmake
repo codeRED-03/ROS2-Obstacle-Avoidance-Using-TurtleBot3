2fe97f50500fdf4470638a4c8546f78a36d68cba
codeRED-03 <ankuromer03@gmail.com>
2023-10-12 16:59:34 +0530

    first

---
+++ build/CMakeFiles/script.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ankur/newRos2dir/src/obstacle_avoidance/src/script.cpp" "CMakeFiles/script.dir/src/script.cpp.o" "gcc" "CMakeFiles/script.dir/src/script.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/script.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/script.dir/src/script.cpp.o"
+  "CMakeFiles/script.dir/src/script.cpp.o.d"
+  "script"
+  "script.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/script.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
